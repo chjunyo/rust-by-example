74056ef16a9f1dec6f963bb021d963cf45cb86a8
chjunyo <101381244+chjunyo@users.noreply.github.com>
2022-05-05 15:23:32 +0900

    4.2. Scope and Shadowing

---
+++ 4. Variable Bindings/2-Scope-and-Shadowing.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let long_lived_binding = 1;
+    {
+        let short_lived_binding = 2;
+        println!("inner short: {}", short_lived_binding); // 2
+    }
+    // println!("outer short: {}", short_lived_binding); // Error
+    println!("outer long: {}", long_lived_binding); // 1
+
+    let shadowed_binding = 1;
+    {
+        println!("before being shadowed: {}", shadowed_binding); // 1
+        let shadowed_binding = "abc";
+        println!("shadowed in inner block: {}", shadowed_binding); // abc
+    }
+    println!("outside inner block: {}", shadowed_binding); // 1
+    let shadowed_binding = 2;
+    println!("shadowed in outer block: {}", shadowed_binding); // 2
+}
