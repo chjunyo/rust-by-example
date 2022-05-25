a505999f17b3efffbfa6c256cdd106847ed9ed04
chjunyo <101381244+chjunyo@users.noreply.github.com>
2022-05-25 18:02:21 +0900

    9.2.5. As output parameters

---
+++ 9. Functions/2-5-As-output-parameters.rs
@@ -0,0 +1,24 @@
+fn create_fn() -> impl Fn() {
+    let text = "Fn".to_owned();
+    move || println!("This is a: {}", text)
+}
+
+fn create_fnmut() -> impl FnMut() {
+    let text = "FnMut".to_owned();
+    move || println!("This is a: {}", text)
+}
+
+fn create_fnonce() -> impl FnOnce() {
+    let text = "FnOnce".to_owned();
+    move || println!("This is a: {}", text)
+}
+
+fn main() {
+    let fn_plain = create_fn();
+    let mut fn_mut = create_fnmut();
+    let fn_once = create_fnonce();
+
+    fn_plain(); // This is a: Fn
+    fn_mut(); // This is a: FnMut
+    fn_once(); // This is a: FnOnce
+}
