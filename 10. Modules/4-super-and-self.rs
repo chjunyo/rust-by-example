6e7010deda3473749edbf010318a21a3db9f0fbe
chjunyo <101381244+chjunyo@users.noreply.github.com>
2022-06-02 20:21:03 +0900

    10.4. super and self

---
+++ 10. Modules/4-super-and-self.rs
@@ -0,0 +1,48 @@
+fn function() {
+    println!("called `function()`");
+}
+
+mod cool {
+    pub fn function() {
+        println!("called `cool::function()`");
+    }
+}
+
+mod my {
+    fn function() {
+        println!("called `my::function()`");
+    }
+    
+    mod cool {
+        pub fn function() {
+            println!("called `my::cool::function()`");
+        }
+    }
+    
+    pub fn indirect_call() {
+        print!("called `my::indirect_call()`, that\n> ");
+        
+        self::function();
+        function();
+        self::cool::function();
+        super::function();
+        
+        {
+            use crate::cool::function as root_function;
+            root_function();
+        }
+    }
+
+    /*
+    called `my::indirect_call()`, that
+    > called `my::function()`
+    called `my::function()`
+    called `my::cool::function()`
+    called `function()`
+    called `cool::function()`
+    */
+}
+
+fn main() {
+    my::indirect_call();
+}
