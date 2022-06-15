e66d78bb8f31e05bae03addb656fd8c9696f78c6
chjunyo <101381244+chjunyo@users.noreply.github.com>
2022-06-15 15:53:39 +0900

    17.1.2. Overload

---
+++ 17. macro_rules!/1-2-Overload.rs
@@ -0,0 +1,18 @@
+macro_rules! test {
+    ($left:expr; and $right:expr) => {
+        println!("{:?} and {:?} is {:?}", stringify!($left), stringify!($right), $left && $right)
+    };
+    ($left:expr; or $right:expr) => {
+        println!("{:?} or {:?} is {:?}", stringify!($left), stringify!($right), $left || $right)
+    };
+}
+
+fn main() {
+    test!(1i32 + 1 == 2i32; and 2i32 * 2 == 4i32);
+    test!(true; or false);
+
+    /*
+    "1i32 + 1 == 2i32" and "2i32 * 2 == 4i32" is true
+    "true" or "false" is true
+    */
+}
